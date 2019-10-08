e740e19995c18b3af0392b3a5d0b3d1b4674ff9f
MathiasGN <math964g@edu.ucl.dk>
2019-10-09 00:26:12 +0200

    Added additional example for local

--- javascript.js
+++ javascript.js
@@ -1,36 +1,58 @@
-let i = 5;
 debugger;
-console.log("Global: " + i);
-debugger;
-
 function local() {
   console.log("Inside local() code block");
-  for (let i = 0; i < 10; i++) {
-    console.log("Local: " + i);
-    // debugger;
-  }
-}
-
-local();
-
-console.log("Outside local() code block");
-
-debugger;
-console.log("Global: " + i);
-debugger;
-
-function localToGlobal() {
-  console.log("Inside localToGlobal() code block");
-  for (i; i < 10; i++) {
+  for (let i = 0; i < 4; i++) {
     console.log("Local: " + i);
     debugger;
   }
 }
 
-localToGlobal();
+  local();
 
-console.log("Outside local() code block");
+  debugger;
 
-debugger;
-console.log("Global: " + i);
-debugger;
+
+
+
+
+  let i = 2;
+  debugger;
+  console.log("Global: " + i);
+  debugger;
+
+  function localUsingGlobal() {
+    console.log("Inside localUsingGlobal() code block");
+    for (let i = 0; i < 4; i++) {
+      console.log("Local: " + i);
+      // debugger;
+    }
+  }
+
+  localUsingGlobal();
+
+  console.log("Outside local() code block");
+
+  debugger;
+  console.log("Global: " + i);
+  debugger;
+
+
+
+
+
+
+  function localToGlobal() {
+    console.log("Inside localToGlobal() code block");
+    for (i; i < 4; i++) {
+      console.log("Local: " + i);
+      debugger;
+    }
+  }
+
+  localToGlobal();
+
+  console.log("Outside local() code block");
+
+  debugger;
+  console.log("Global: " + i);
+  debugger;
